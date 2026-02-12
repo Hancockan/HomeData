ef615bf75424cbc8e57d081540b3f77172a9dd97
Andrew Hancock <andrewhancock0@gmail.com>
2026-02-11 21:36:14 -0600

    Send data to server

--- src/main.py
+++ src/main.py
@@ -1,6 +1,10 @@
 import adafruit_dht
 import board
 import time
+import socket
+import struct
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 dht = adafruit_dht.DHT22(board.D4)
 
@@ -8,7 +12,11 @@ while True:
 	try:
 		temp = dht.temperature
 		hum = dht.humidity
-		print((temp * (9/5)) + 32 )
+		temp = (temp * (9/5) + 32) 
+		print(temp)
+
+		temp = struct.pack('!f', temp)
+		sock.sendto(temp, ("192.168.86.29", 35423))
 	except RuntimeError as e:
 		print("error")
 	time.sleep(2)
